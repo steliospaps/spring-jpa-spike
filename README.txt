813d16279dbadecbfdd9c98e0f9beea0f718c860
stelios <stelios05@gmail.com>
2016-06-25 21:36:36 +0000

    flyway migration happens outside of process

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 mvn clean spring-boot:run
 mysql -uroot -proot test -e 'select * from ITEM;'
  mvn spring-boot:run -Dspring.profiles.active=mysql
+mvn flyway:migrate -Dflyway.url=jdbc:mysql://localhost/test -Dflyway.user=root -Dflyway.password=root
--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,9 +3,12 @@ package hello;
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -32,6 +35,17 @@ public class Application {
 		return new DataSourceTransactionManager(dataSource);
 	}
 */
+	@ConditionalOnProperty(name="flyway.disable-automatic-migration")
+	@Bean
+	FlywayMigrationStrategy flywayMigrationStrategy() {
+		return new FlywayMigrationStrategy() {
+			
+			@Override
+			public void migrate(Flyway flyway) {
+				flyway.validate();
+			}
+		};
+	}
 	@Bean
 	public CommandLineRunner demo(ItemRepository rep) {
 		return (args) -> {
